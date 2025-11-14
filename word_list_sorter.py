16e31c00a7c2909353b5a3257d9b981a4f74b11e
seangupta1 <seangupta1@outlook.com>
2025-11-13 22:47:06 -0600

    Update word_list_sorter.py

--- word_list_sorter.py
+++ word_list_sorter.py
@@ -1,12 +1,17 @@
-filepath = 'HW 3/1000-most-common-words.txt'
+# Original file
+filepath = '1000-most-common-words.txt'
 
+# Empty list
 words = []
 
+# Load in words from original file and add to list
 with open(filepath, 'r') as file:
     for line in file:
         words.append(line.rstrip())
 
+# Sort list
 words.sort()
 
+# Print index and word
 for element in words:
     print(f"{words.index(element)} | {element}")
